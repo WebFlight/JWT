b3a27e8878a757eb17f72c04e06adad1628d0672
Menno de Haas <menno.dehaas@webflight.nl>
2018-06-08 14:29:16 +0200

    Removed try catch clauses in RSAKeyPairGenerator

--- javasource/jwt/helpers/RSAKeyPairGenerator.java
+++ javasource/jwt/helpers/RSAKeyPairGenerator.java
@@ -15,48 +15,35 @@ import com.mendix.systemwideinterfaces.core.IContext;
 
 public class RSAKeyPairGenerator {
 	
-	public jwt.proxies.KeyPair generate(IContext context, int keySize) {
-		try {
-			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-			keyPairGenerator.initialize(keySize);
-			KeyPair keyPair = keyPairGenerator.genKeyPair();
-			
-			RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-			RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-			
-			jwt.proxies.KeyPair keyPairObject = new jwt.proxies.KeyPair(context);
-			
-			jwt.proxies.PublicKey publicKeyObject = new jwt.proxies.PublicKey(context);
-			publicKeyObject.setPublicKey_KeyPair(context, keyPairObject);
-			Core.commit(context, publicKeyObject.getMendixObject());
-			
-			jwt.proxies.PrivateKey privateKeyObject = new jwt.proxies.PrivateKey(context);
-			privateKeyObject.setPrivateKey_KeyPair(context, keyPairObject);
-			Core.commit(context, privateKeyObject.getMendixObject());
-			
-			Core.commit(context, keyPairObject.getMendixObject());
-			
-			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
-					publicKey.getEncoded());
-			
-			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
-					privateKey.getEncoded());
-			
-			Core.storeFileDocumentContent(context, publicKeyObject.getMendixObject(), "public" + keyPairObject.getKeyPairId(context) + ".key", new ByteArrayInputStream(x509EncodedKeySpec.getEncoded()));
-			Core.storeFileDocumentContent(context, privateKeyObject.getMendixObject(), "private" + keyPairObject.getKeyPairId(context) + ".key", new ByteArrayInputStream(pkcs8EncodedKeySpec.getEncoded()));
-			
-			return keyPairObject;
-		} catch (NoSuchAlgorithmException e) {
-			
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public jwt.proxies.KeyPair generate(IContext context, int keySize) throws NoSuchAlgorithmException, CoreException {
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(keySize);
+		KeyPair keyPair = keyPairGenerator.genKeyPair();
 		
-		return null;
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		
+		jwt.proxies.KeyPair keyPairObject = new jwt.proxies.KeyPair(context);
+		
+		jwt.proxies.PublicKey publicKeyObject = new jwt.proxies.PublicKey(context);
+		publicKeyObject.setPublicKey_KeyPair(context, keyPairObject);
+		Core.commit(context, publicKeyObject.getMendixObject());
+		
+		jwt.proxies.PrivateKey privateKeyObject = new jwt.proxies.PrivateKey(context);
+		privateKeyObject.setPrivateKey_KeyPair(context, keyPairObject);
+		Core.commit(context, privateKeyObject.getMendixObject());
+		
+		Core.commit(context, keyPairObject.getMendixObject());
+		
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+				publicKey.getEncoded());
+		
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+				privateKey.getEncoded());
+		
+		Core.storeFileDocumentContent(context, publicKeyObject.getMendixObject(), "public" + keyPairObject.getKeyPairId(context) + ".key", new ByteArrayInputStream(x509EncodedKeySpec.getEncoded()));
+		Core.storeFileDocumentContent(context, privateKeyObject.getMendixObject(), "private" + keyPairObject.getKeyPairId(context) + ".key", new ByteArrayInputStream(pkcs8EncodedKeySpec.getEncoded()));
+		
+		return keyPairObject;	
 	}
-	
-	
 }
