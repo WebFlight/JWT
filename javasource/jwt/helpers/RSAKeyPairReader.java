01139e48a62f8ca1343f9d151fb61f0eee94c3f1
unknown <braakpn@gmail.com>
2018-10-31 00:23:02 +0100

    Removed commons io dependency

--- javasource/jwt/helpers/RSAKeyPairReader.java
+++ javasource/jwt/helpers/RSAKeyPairReader.java
@@ -1,5 +1,6 @@
 package jwt.helpers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyFactory;
@@ -9,30 +10,38 @@ import java.security.PublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-
-import org.apache.commons.io.IOUtils;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
 import com.mendix.core.Core;
 import com.mendix.systemwideinterfaces.core.IContext;
 
 import jwt.proxies.JWTRSAPrivateKey;
 import jwt.proxies.JWTRSAPublicKey;
-import sun.misc.IOUtils;
 
 public class RSAKeyPairReader {
 	
 	public RSAPublicKey getPublicKey(IContext context, JWTRSAPublicKey publicKeyObject) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-		InputStream inputStream = Core.getFileDocumentContent(context, publicKeyObject.getMendixObject());
-		byte[] encodedPublicKey = IOUtils.toByteArray(inputStream);
+		try(InputStream inputStream = Core.getFileDocumentContent(context, publicKeyObject.getMendixObject())){
 		
+		//byte[] encodedPublicKey = IOUtils.toByteArray(inputStream);
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+	    int nRead;
+	    byte[] data = new byte[1024];
+	    while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+	        buffer.write(data, 0, nRead);
+	    }
+	 
+	    buffer.flush();
+	    byte[] encodedPublicKey = buffer.toByteArray();
+			
 		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 		X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublicKey);
 		PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
 		
 		return (RSAPublicKey) publicKey;
+		}
+		
 	}
 	
 	public RSAPrivateKey getPrivateKey(IContext context, JWTRSAPrivateKey privateKeyObject) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
@@ -40,11 +49,11 @@ public class RSAKeyPairReader {
 		byte[] encodedPrivateKey = new byte[inputStream.available()];
 		inputStream.read(encodedPrivateKey);
 		
-		PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(encodedPrivateKey);
-		
-		JcaPEMKeyConverter jcaPEMKeyConverter = new JcaPEMKeyConverter();
-		PrivateKey privateKey = jcaPEMKeyConverter.getPrivateKey(privateKeyInfo);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
+		PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
 		
 		return (RSAPrivateKey) privateKey;
 	}
+	
 }
